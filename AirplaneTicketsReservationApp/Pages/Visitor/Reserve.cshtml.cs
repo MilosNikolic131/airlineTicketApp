1b95156bdceb2a106848981f0fab1fa58c68a6c2
Miloš Nikolić <Milos.Nikolic@be-terna.com>
2023-05-29 12:00:12 +0000

    signalR

---
+++ AirplaneTicketsReservationApp/Pages/Visitor/Reserve.cshtml.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AirplaneTicketsReservationApp.Models;
+using System.Data.SqlClient;
+using AirplaneTicketsReservationApp.Pages.Flights;
+using Microsoft.AspNetCore.SignalR;
+using AirplaneTicketsReservationApp.Hubs;
+
+namespace AirplaneTicketsReservationApp.Pages.Visitor
+{
+    public class ReserveModel : PageModel
+    {
+
+        public Reservation reservation = new Reservation();
+        public IHubContext<ReservationHub> hubContext;
+        public void OnGet()
+        {
+            String id = Request.Query["id"];
+
+            try
+            {
+                string connectionString = "Data Source=.\\SQLEXPRESS2;Initial Catalog=airlineDB;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    try
+                    {
+                        String sql2 = "SELECT * FROM flight";
+                        using (SqlCommand command2 = new SqlCommand(sql2, connection))
+                        {
+                            using (SqlDataReader reader = command2.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    reservation.flightId = reader.GetInt32(0);
+                                  
+                                }
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        return;
+                    }
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+        }
+
+        public async Task OnPostAsync()
+        {
+            reservation.flightId = int.Parse(Request.Form["flightId"]);
+            reservation.userId = int.Parse(Request.Form["userId"]);
+            reservation.numberOfSeats = int.Parse(Request.Form["numberOfSeats"]);
+            reservation.approved = int.Parse("0");
+
+            string connectionString = "Data Source=.\\SQLEXPRESS2;Initial Catalog=airlineDB;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                try
+                {
+                    String sql2 = "INSERT INTO reservation " +
+                        "(idFlight, idUser, numberOfSeats, approved)"
+                        + " VALUES " + "(@idFlight, @idUser, @numberOfSeats, @approved);";
+                    using (SqlCommand command2 = new SqlCommand(sql2, connection))
+                    {
+                        command2.Parameters.AddWithValue("@idFlight", reservation.flightId);
+                        command2.Parameters.AddWithValue("@idUser", reservation.userId);
+                        command2.Parameters.AddWithValue("@numberOfSeats", reservation.numberOfSeats);
+                        command2.Parameters.AddWithValue("@approved", reservation.approved);
+
+
+                        command2.ExecuteNonQuery();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+
+            }
+
+           await hubContext.Clients.All.SendAsync("NewReservationReceived", reservation);
+
+        }
+    }
+}
--- AirplaneTicketsReservationApp/wwwroot/js/ReservationHub.js
+++ AirplaneTicketsReservationApp/wwwroot/js/ReservationHub.js
@@ -1,10 +1,49 @@
 ﻿"use strickt";
 
+const { signalR } = require("../lib/@aspnet/signalr/dist/browser/signalr");
+
 var connection = new SignalR.HubConnectionBuilder().withUrl("/reservationhub")
     .configureLogging(SignalR.LogLevel.Information).build();
 
+/*$(document).ready(() => {*/
+
+    let reservations = [];
+    let $resBody = $("resBody");
+
+    function approve(button) {
+        let id = $(button).attr("id");
+
+    }
+
+    const client = new signalR.HubConnectionBuilder().withUrl("/reservationhub")
+        .configureLogging(SignalR.LogLevel.Information).build();
+
+    client.on("NewReservationReceived", newReservation => {
+        addReservation(newReservation);
+    })
+
+    function addReservations() {
+        $resBody.empty();
+        $.each(reservations, (i, r) => addReservation(r));
+
+    }
+
+    function addReservation(res) {
+        let $template =
+            <tr>
+                <td>${res.flightId}</td>
+                <td>${res.userId}</td>
+                <td>${res.numberOfSeats}</td>
+                <td>${res.approved}</td>
+            </tr>;
+        $resBody.append($template);
+    }
+
+
+/*})*/
+
 connection.on("NewReservationReceived", function (_reservation) {
-    document.getElementById("FlightID").value = _reservation;
+    addReservation(newReservation);
 });
 
 connection.start().catch(function err() {
